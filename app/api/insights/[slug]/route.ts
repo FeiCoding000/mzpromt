ec5d415d8d8c6a9cb99a49c253fa6538e04dbd4d
feicoding888 <weihaianni@gmail.com>
2026-06-04 06:35:21 +1000

    Fix: change query post by slug to dynamic router

---
+++ app/api/insights/[slug]/route.ts
@@ -0,0 +1,15 @@
+import { getPostBySlugService } from "@/lib/services/postService";
+import { NextResponse } from "next/dist/server/web/spec-extension/response";
+
+export async function GET(
+    request: Request,
+    { params }: { params: Promise<{ slug: string }> }
+) {
+    const { slug } = await params;
+
+    const post = await getPostBySlugService(slug);
+    if (!post) {
+        return NextResponse.json({ ok: false, error: "Post not found" }, { status: 404 });
+    }
+    return NextResponse.json(post);
+}
